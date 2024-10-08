2f1b7387b96b5a8c7fb50b1a22c89aa220b8089a
sarthakbreathe <sarthaktadas918@gmail.com>
2024-10-08 12:01:39 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const { connectToMongoDB } = require('./connect');
 // Connect to MongoDB
 connectToMongoDB(process.env.MONGO_URL);
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://shopshereeeee.vercel.app/', credentials: true }));
 app.use(fileUpload({ useTempFiles: true }));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
